e5ed7dc32b9816271bf5eb43f108ceef44cd31e9
DevPed <pedromfr91@gmail.com>
2021-10-18 01:35:40 +0200

    Slides and Dots

---
+++ script.js
@@ -0,0 +1,19 @@
+const slides = document.querySelectorAll('.slides');
+const dots = document.querySelectorAll('.dot');
+
+function SetActive(i) {
+  for (slide of slides) {
+    slide.classList.remove('active');
+    slides[i].classList.add('active');
+  }
+  //active class for dots
+  for (dot of dots) {
+    dot.classList.remove('active');
+    dots[i].classList.add('active');
+  }
+}
+for (let j = 0; j < dots.length; j++) {
+  dots[j].addEventListener('click', function () {
+    SetActive(j);
+  });
+}
